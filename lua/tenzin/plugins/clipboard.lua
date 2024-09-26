fe0916044f2bfe69a5fa394e15553a6316f70343
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2024-09-26 13:59:00 +1000

    Made osc52 work with tmux

--- lua/tenzin/plugins/clipboard.lua
+++ lua/tenzin/plugins/clipboard.lua
@@ -4,5 +4,9 @@ return {
 		vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
 		vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
 		vim.keymap.set('x', '<leader>c', require('osc52').copy_visual)
+
+		require("osc52").setup({
+			tmux_passthrough = true,
+		})
 	end
 }
